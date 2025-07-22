1d8d62db60fe3b30c26d091f99d4ee2bf3bf7419
mohammad iko <developer.iko.mike@gmail.com>
2025-07-21 18:51:15 -0700

    add Episode 19

--- src/Episodes/E19.tsx
+++ src/Episodes/E19.tsx
@@ -3,9 +3,21 @@ import React from 'react'
 type Props = {}
 
 const E19 = (props: Props) => {
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {console.log(e);}
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {console.log(e);}
+
+
   return (
-    <div>E19</div>
+    <div className='fullCenterCol'>
+        <button onClick={handleClick} className='bg-blue-500 text-white p-2'>click me</button>
+        {/* <button onClick={e => {}} className='bg-blue-500 text-white p-2'>click me</button> */}
+        <input type="text" onChange={handleChange}/>
+        {/* <input type="text" onChange={e => {}}/> */}
+    </div>
   )
 }
 
 export default E19;
+
+// برای فهمیدن تایپ ایونت هر پراپ المنت فقط کافیه توی همون اکولاد ان کلیک ایونت رو از ورودی بگیریم و روش هاور کنیم و به ما اسم ایونت رو میگه
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/reset.css'
 import './styles/index.css'
-import App from './Episodes/E18'
+import App from './Episodes/E19'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
